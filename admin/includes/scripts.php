eb845e22af89c507036a8c5eec24250fedd71f11
Haris123456788 <waseemharis1122@gmail.com>
2024-10-01 18:20:40 +0500

    Insertion

--- admin/includes/scripts.php
+++ admin/includes/scripts.php
@@ -14,3 +14,7 @@
     <!-- Page level custom scripts -->
     <script src="js/demo/chart-area-demo.js"></script>
     <script src="js/demo/chart-pie-demo.js"></script>
+     <!-- Bootstrap JS and dependencies -->
+     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
