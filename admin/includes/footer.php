e8d1d746a20e76f950c60c4fe33282e9472c5c2c
Haris123456788 <waseemharis1122@gmail.com>
2024-10-01 19:11:22 +0500

    Insertion changes

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -4,7 +4,7 @@
              <footer class="sticky-footer bg-white">
                 <div class="container my-auto">
                     <div class="copyright text-center my-auto">
-                        <span>Copyright &copy; Your Website 2021</span>
+                        <span>Copyright &copy; Project X9 Gaming</span>
                     </div>
                 </div>
             </footer>
